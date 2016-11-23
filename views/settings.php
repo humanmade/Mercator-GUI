33ef0424381d463bf789ad1c1c4ed4d3995c4a00
Robert O'Rourke <robert@o-rourke.org>
2016-11-23 16:37:40 -0500

    allow for defining some constants to provide help text o nthe domains page

--- views/settings.php
+++ views/settings.php
@@ -6,7 +6,20 @@
 
 <div class="wrap">
 	<h1><?php esc_html_e( 'Domain settings', 'mercator' ); ?></h1>
-	<p class="intro"><?php esc_html_e( 'You can add and edit aliases here.' ); ?></p>
+	<?php if ( defined( 'MERCATOR_DNS_CNAME' ) ) : ?>
+		<p><?php printf( esc_html__( 'Custom domains need a %s record pointing to %s' ),
+		                 '<code>CNAME</code>',
+		                 '<code>' . MERCATOR_DNS_CNAME . '</code>' ); ?>
+		</p>
+		<p><?php esc_html_e( 'If you\'re not sure what to do get in touch with the registrar you purchased your domain from.' ); ?></p>
+	<?php endif; ?>
+	<?php if ( defined( 'MERCATOR_DNS_A' ) ) : ?>
+		<p><?php printf( esc_html__( 'Custom domains need an %s record pointing to %s' ),
+		                 '<code>A</code>',
+		                 '<code>' . MERCATOR_DNS_A . '</code>' ); ?>
+		</p>
+		<p><?php esc_html_e( 'If you\'re not sure what to do get in touch with the registrar you purchased your domain from.' ); ?></p>
+	<?php endif; ?>
 	<div class="mercator-gui"></div>
 </div>
 
@@ -19,19 +32,26 @@
 <script type="text/template" id="tmpl-mercator-primary-domain">
 	<h2><?php esc_html_e( 'Primary domain', 'mercator' ); ?></h2>
 	<p class="mercator-primary-domain-value"><%= domain %></p>
-	<hr />
+	<hr/>
 </script>
 <script type="text/template" id="tmpl-mercator-alias">
-	<input class="mercator-alias-domain" type="text" placeholder="<?php esc_attr_e( 'example.com, hit Enter to save', 'mercator' ); ?>" pattern="([a-z0-9]+\.)?([a-z0-9_-]+\.[a-z]+)(\.[a-z]+)" value="<%- domain %>" />
+	<input class="mercator-alias-domain" type="text"
+	       placeholder="<?php esc_attr_e( 'example.com, hit Enter to save', 'mercator' ); ?>"
+	       pattern="([a-z0-9]+\.)?([a-z0-9_-]+\.[a-z]+)(\.[a-z]+)" value="<%- domain %>"/>
 	<label class="mercator-alias-active-label">
 		<input class="mercator-alias-active" type="checkbox" <%= active ? 'checked' : '' %> />
 		<?php esc_html_e( 'Active', 'mercator' ); ?>
 	</label>
 	<div class="mercator-alias-controls">
-		<button class="mercator-alias-update button button-secondary"><?php esc_html_e( 'Update', 'mercator' ); ?></button>
-		<button class="mercator-alias-delete button button-secondary"><?php esc_html_e( 'Delete', 'mercator' ); ?></button>
-		<button class="mercator-alias-ays button button-secondary" style="display:none"><?php esc_html_e( 'Are you sure?', 'mercator' ); ?></button>
-		<button class="mercator-alias-cancel button button-secondary" style="display:none"><?php esc_html_e( 'Cancel', 'mercator' ); ?></button>
-		<button class="mercator-alias-primary button button-secondary"><?php esc_html_e( 'Make primary', 'mercator' ); ?></button>
+		<button
+			class="mercator-alias-update button button-secondary"><?php esc_html_e( 'Update', 'mercator' ); ?></button>
+		<button
+			class="mercator-alias-delete button button-secondary"><?php esc_html_e( 'Delete', 'mercator' ); ?></button>
+		<button class="mercator-alias-ays button button-secondary"
+		        style="display:none"><?php esc_html_e( 'Are you sure?', 'mercator' ); ?></button>
+		<button class="mercator-alias-cancel button button-secondary"
+		        style="display:none"><?php esc_html_e( 'Cancel', 'mercator' ); ?></button>
+		<button
+			class="mercator-alias-primary button button-secondary"><?php esc_html_e( 'Make primary', 'mercator' ); ?></button>
 	</div>
 </script>
